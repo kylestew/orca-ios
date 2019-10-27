bfd261e12ea8cbb95c187e1066685178a9c95ee2
Kyle Stewart <kylestew@gmail.com>
2019-10-26 18:55:33 -0700

    Epic battle with xcode to allow me to create a project compatible with iOS 12

---
+++ orca-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  orca-ios
+//
+//  Created by Kyle Stewart on 10/26/19.
+//  Copyright © 2019 Kyle Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
