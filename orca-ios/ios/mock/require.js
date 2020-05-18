4033c8efc2ea5e850cdfb5d27966aaaa2c8a0ccb
Kyle Stewart <kylestew@gmail.com>
2020-05-18 10:42:24 -0700

    MIDI working, a lot of stuff is no longer stubbed out
    Trying to follow how ORCA web works

--- orca-ios/ios/mock/require.js
+++ orca-ios/ios/mock/require.js
@@ -1,7 +1,37 @@
+'use strict'
+
+// forward console.log to iOS wrapper for xcode log output
+console.log = function(what) {
+    window.webkit.messageHandlers.debugLog.postMessage("LOG: " + JSON.stringify(arguments));
+};
+console.info = function(what) {
+    window.webkit.messageHandlers.debugLog.postMessage("INFO: " + JSON.stringify(arguments));
+};
+console.warn = function(what) {
+    window.webkit.messageHandlers.debugLog.postMessage("WARN: " + JSON.stringify(arguments));
+};
+console.error = function(what) {
+    window.webkit.messageHandlers.debugLog.postMessage("ERROR: " + JSON.stringify(arguments));
+};
+
+
+//import * as electron from './electron.js'
+
+// require() is a node thing and doesn't exist in the browser
+// let's make a fake one, nothing can go wrong ;)
+//window.mockRequire = function(name) {
+//    switch (name) {
+//////        case 'electron': return electron; break
+//        default: console.warn('Failed to require ' + name);
+//    }
+//}
+
+
+/*
 import * as fs from './fs.js'
 import * as path from './path.js'
+
 import * as osc from './osc.js'
-import * as electron from './electron.js'
 import * as dgram from './dgram.js'
 
 import Buffer from './buffer.js'
@@ -17,14 +47,5 @@ window.require = function (what) {
     default: console.log("missing mock - " + what); break
   }
 }
-
-// forward console.log to iOS wrapper for xcode log output
-console.log = function(what) {
-    window.webkit.messageHandlers.debugLog.postMessage("LOG: " + JSON.stringify(arguments));
-};
-console.warn = function(what) {
-    window.webkit.messageHandlers.debugLog.postMessage("WARN: " + JSON.stringify(arguments));
-};
-console.error = function(what) {
-    window.webkit.messageHandlers.debugLog.postMessage("ERROR: " + JSON.stringify(arguments));
-};
+*/
+console.info("Electron mock installed");
